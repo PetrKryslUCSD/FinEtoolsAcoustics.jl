26ba86ad789ba4d788d864f475ebebf4a71e899a
Petr Krysl <pkrysl@ucsd.edu>
2023-11-20 07:44:57 -0800

    customize the pool functions

--- examples/transient/transdec_pool_transient_examples.jl
+++ examples/transient/transdec_pool_transient_examples.jl
@@ -5,151 +5,16 @@ using FinEtools.MeshExportModule.VTKWrite: vtkwritecollection, vtkwrite
 using FinEtoolsAcoustics
 using DataDrop
 using LinearAlgebra
-using PlotlyLight
+# using PlotlyLight
+using Plots
 
-function transdec_pool_harmonic(freq = 100)
-	rho = 1000.0*phun("kg/m^3");# mass density
-        c  = 1500.0*phun("m/s");# sound speed
-	bulk =  c^2*rho;
-	omega =  freq*phun("rev/s");      # frequency of the piston
-	P_piston = 1.0e-3*phun("MPa"); # amplitude of the piston pressure
-	R = 0.5*phun("ft");# radius of the piston
-    rim_distance = 150 * phun("ft")
-	tolerance = R/100
-	dt = 1.0/freq/20;
-	tfinal = rim_distance / c  * 3
-	nsteps = Int(round(tfinal/dt))+1;
-    nbtw = max(1, Int(round(nsteps/300)))
-    pos_pressure_fun(t) = P_piston*sin(omega*t)
-    pos_pressuredd_fun(t) = (-omega^2)*pos_pressure_fun(t)
-    neg_pressure_fun(t) = P_piston*sin(omega*t)
-    neg_pressuredd_fun(t) = (-omega^2)*neg_pressure_fun(t)
+function _pressure_pulse(P_piston, omega, t)
+    (t <= 2*pi/omega) ? P_piston*sin(omega*t) : 0.0
+end
 
-    t0  =  time()
-
-    # Hexahedral mesh
-    mesh = import_ABAQUS(joinpath(@__DIR__, "transdec-pool-quarter.inp"))
-    fens = mesh["fens"]
-    fes = mesh["fesets"][1]
-
-    fens.xyz *= phun("ft")
-
-    @info "$(count(fens)) nodes"
-
-    fens1, fes1 = mirrormesh(fens, fes, [-1.0, 0.0, 0.0], [0.0, 25.0, 56.65].*phun("ft"); renumb =  c ->  c[[1, 3, 2, 4]])
-    fens, newfes1, fes2 =  mergemeshes(fens1, fes1, fens, fes, tolerance)
-    fes = cat(newfes1, fes2)
-
-    bfes  =  meshboundary(fes)
-    File  =   "transdec-pool-boundary.vtk"
-    vtkexportmesh(File, fens, bfes)
-    # @async run(`"paraview.exe" $File`)
-
-    l1 = selectelem(fens, bfes, facing = true, direction = [-1.0 0.0 0.0])
-    l2 = selectelem(fens, bfes, facing = true, direction = [+1.0 0.0 0.0])
-    l3 = selectelem(fens, bfes, distance = R, from = [0.0, 6.0, 56.65].*phun("ft"), inflate = tolerance)
-    l4 = selectelem(fens, bfes, box = [-Inf, Inf, 25.0, 25.0, -Inf, Inf].*phun("ft"), inflate = tolerance)
-
-    lpos = intersect(l2, l3)
-    lneg = intersect(l1, l3)
-
-    File  =   "transdec-pool-pos.vtk"
-    vtkexportmesh(File, fens, subset(bfes, lpos))
-
-    File  =   "transdec-pool-neg.vtk"
-    vtkexportmesh(File, fens, subset(bfes, lneg))
-
-    File  =   "transdec-pool-free.vtk"
-    vtkexportmesh(File, fens, subset(bfes, l4))
-
-    # Piston surface mesh
-    piston_pos_fes = subset(bfes, lpos);
-    piston_neg_fes = subset(bfes, lneg);
-
-    println("Pre-processing time elapsed  =  ",time() - t0,"s")
-
-	t1  =  time()
-
-	material = MatAcoustFluid(bulk, rho)
-	# Region of the fluid
-	femm = FEMMAcoust(IntegDomain(fes, TetRule(1)), material)
-
-	geom = NodalField(fens.xyz);
-	P = NodalField(zeros(nnodes(geom),1));
-	piston_pos_fenids = connectednodes(piston_pos_fes);
-	setebc!(P, piston_pos_fenids, true, 1, 0.0);
-    piston_neg_fenids = connectednodes(piston_neg_fes);
-    setebc!(P, piston_neg_fenids, true, 1, 0.0);
-    setebc!(P, connectednodes(subset(bfes, l4)), true, 1, 0.0);
-	applyebc!(P);
-	numberdofs!(P);
-
-	S  =  acousticstiffness(femm, geom, P);
-	C = acousticmass(femm, geom, P);
-	D = (2.0/dt)*S +(dt/2.0)*C;
-
-	P0 = deepcopy(P)
-	Pdd0 = deepcopy(P)
-	P1 = deepcopy(P)
-	Pdd1 = deepcopy(P)
-	TMPF = deepcopy(P)
-	P = nothing # we don't need this field anymore
-	vP0 = zeros(P0.nfreedofs)
-	vP1 = deepcopy(vP0)
-	vPd0 = deepcopy(vP0)
-	vPd1 = deepcopy(vP0)
-
-	t = 0.0
-	P0.values[piston_pos_fenids,1] .= pos_pressure_fun(t)
-	Pdd0.values[piston_pos_fenids,1] .= pos_pressuredd_fun(t)
-    P0.values[piston_neg_fenids,1] .= neg_pressure_fun(t)
-    Pdd0.values[piston_neg_fenids,1] .= neg_pressuredd_fun(t)
-	vP0 = gathersysvec!(P0, vP0)
-
-	# nh = selectnode(fens, nearestto = [R+Ro/2, 0.0, 0.0] )
-	# Pnh = [P1.values[nh, 1][1]]
-	pressures = [(t, vP1)]
-	step = 0;
-	while t <=tfinal
-		step = step  +1;
-		t=t+dt;
-		P1.values[piston_pos_fenids,1] .= pos_pressure_fun(t)
-		Pdd1.values[piston_pos_fenids,1] .= pos_pressuredd_fun(t)
-        P1.values[piston_neg_fenids,1] .= neg_pressure_fun(t)
-        Pdd1.values[piston_neg_fenids,1] .= neg_pressuredd_fun(t)
-		TMPF.values = P0.values + P1.values
-		F = nzebcloadsacousticmass(femm, geom, TMPF);
-		TMPF.values = Pdd0.values + Pdd1.values
-		F = F + nzebcloadsacousticstiffness(femm, geom, TMPF);
-		# println("$(norm(F))")
-		vPd1 = D\((2/dt)*(S*vPd0) - C*(2*vP0+(dt/2)*vPd0) + F);
-		vP1 = vP0 + (dt/2)*(vPd0+vPd1);
-		scattersysvec!(P1, vP1); # store current pressure
-        if rem(step+1, nbtw) == 0
-            push!(pressures, (t, deepcopy(vP1)))
-        end
-        # Swap variables for the next step
-		copyto!(vP0, vP1)
-		copyto!(vPd0, vPd1)
-		copyto!(P0, P1)
-		copyto!(Pdd0, Pdd1)
-		println("step = $( step )/$(nsteps)")
-	end
-
-    # Visualization
-    @info "Dumping visualization"
-    times = Float64[]
-    scalars = []
-    for i in 1:length(pressures)
-        t, p = pressures[i]
-        scattersysvec!(P1, p)
-        push!(scalars, ("p", deepcopy(P1.values[:, 1])))
-        push!(times, t)
-    end
-    vtkwritecollection("transdec_pool_harmonic-f=$(freq)-p", fens, fes, times; scalars = scalars)
-
-	true
-end #
+function _pressure_cw(P_piston, omega, t)
+    P_piston*sin(omega*t)
+end
 
 function transect_interpolation(fens, fes, from, to, npoints, geometricaltolerance)
     parametrictolerance = 1.0e-5
@@ -185,9 +50,9 @@ function transect_interpolation(fens, fes, from, to, npoints, geometricaltoleran
     return distances, interp
 end
 
-function transdec_pool_pulse(freq = 100)
+function _run_transdec_pool(name, freq, pos_pressure_fun, neg_pressure_fun, nperiods)
     rho = 1000.0*phun("kg/m^3");# mass density
-        c  = 1500.0*phun("m/s");# sound speed
+    c  = 1500.0*phun("m/s");# sound speed
     bulk =  c^2*rho;
     omega =  freq*phun("rev/s");      # frequency of the piston
     P_piston = 1.0e-3*phun("MPa"); # amplitude of the piston pressure
@@ -195,13 +60,13 @@ function transdec_pool_pulse(freq = 100)
     rim_distance = 150 * phun("ft")
     tolerance = R/100
     dt = 1.0/freq/20;
-    tfinal = rim_distance / c  * 0.2
+    tfinal = rim_distance / c  * nperiods
     nsteps = Int(round(tfinal/dt))+1;
     nbtw = max(1, Int(round(nsteps/300)))
-    pos_pressure_fun(t) = (t <= 1/freq) ? P_piston*sin(omega*t) : 0.0
-    pos_pressuredd_fun(t) = (-omega^2)*pos_pressure_fun(t)
-    neg_pressure_fun(t) = (t <= 1/freq) ? P_piston*sin(omega*t) : 0.0
-    neg_pressuredd_fun(t) = (-omega^2)*neg_pressure_fun(t)
+    _pos_pressure_fun(t) = pos_pressure_fun(P_piston, omega, t)
+    _pos_pressuredd_fun(t) = (-omega^2)*_pos_pressure_fun(t)
+    _neg_pressure_fun(t) = neg_pressure_fun(P_piston, omega, t)
+    _neg_pressuredd_fun(t) = (-omega^2)*_neg_pressure_fun(t)
 
     t0 = time()
 
@@ -219,7 +84,7 @@ function transdec_pool_pulse(freq = 100)
     fes = cat(newfes1, fes2)
 
     bfes = meshboundary(fes)
-    File =  "transdec-pool-boundary.vtk"
+    File =  "$(name)-boundary.vtk"
     vtkexportmesh(File, fens, bfes)
     # @async run(`"paraview.exe" $File`)
 
@@ -231,13 +96,13 @@ function transdec_pool_pulse(freq = 100)
     lpos = intersect(l2, l3)
     lneg = intersect(l1, l3)
 
-    File =  "transdec-pool-pos.vtk"
+    File =  "$(name)-pos.vtk"
     vtkexportmesh(File, fens, subset(bfes, lpos))
 
-    File =  "transdec-pool-neg.vtk"
+    File =  "$(name)-neg.vtk"
     vtkexportmesh(File, fens, subset(bfes, lneg))
 
-    File =  "transdec-pool-free.vtk"
+    File =  "$(name)-free.vtk"
     vtkexportmesh(File, fens, subset(bfes, l4))
 
     # Piston surface mesh
@@ -283,10 +148,10 @@ function transdec_pool_pulse(freq = 100)
     vTMP_d = gathersysvec(P0, :d)
 
     t = 0.0
-    P0.values[piston_pos_fenids,1] .= pos_pressure_fun(t)
-    Pdd0.values[piston_pos_fenids,1] .= pos_pressuredd_fun(t)
-    P0.values[piston_neg_fenids,1] .= neg_pressure_fun(t)
-    Pdd0.values[piston_neg_fenids,1] .= neg_pressuredd_fun(t)
+    P0.values[piston_pos_fenids,1] .= _pos_pressure_fun(t)
+    Pdd0.values[piston_pos_fenids,1] .= _pos_pressuredd_fun(t)
+    P0.values[piston_neg_fenids,1] .= _neg_pressure_fun(t)
+    Pdd0.values[piston_neg_fenids,1] .= _neg_pressuredd_fun(t)
     vP0 = gathersysvec!(P0, vP0)
 
     # nh = selectnode(fens, nearestto = [R+Ro/2, 0.0, 0.0] )
@@ -296,10 +161,10 @@ function transdec_pool_pulse(freq = 100)
     while t <=tfinal
         step = step  +1;
         t=t+dt;
-        P1.values[piston_pos_fenids,1] .= pos_pressure_fun(t)
-        Pdd1.values[piston_pos_fenids,1] .= pos_pressuredd_fun(t)
-        P1.values[piston_neg_fenids,1] .= neg_pressure_fun(t)
-        Pdd1.values[piston_neg_fenids,1] .= neg_pressuredd_fun(t)
+        P1.values[piston_pos_fenids,1] .= _pos_pressure_fun(t)
+        Pdd1.values[piston_pos_fenids,1] .= _pos_pressuredd_fun(t)
+        P1.values[piston_neg_fenids,1] .= _neg_pressure_fun(t)
+        Pdd1.values[piston_neg_fenids,1] .= _neg_pressuredd_fun(t)
         TMPF.values = P0.values + P1.values
         F_f .= - C_fd * gathersysvec!(TMPF, vTMP_d, :d)
         TMPF.values = Pdd0.values + Pdd1.values
@@ -322,16 +187,10 @@ function transdec_pool_pulse(freq = 100)
     @info "Dumping visualization"
 
     from = [-0.01524000021336, 1.8288000000000002, 17.2669204572]
-    to = from .+ [-1.5, 0, 0]
-    npoints = 10
+    to = from .+ [-7.5, 0, 0]
+    npoints = 50
     geometricaltolerance = tolerance / 100
     distances, interp = transect_interpolation(fens, fes, from, to, npoints, geometricaltolerance)
-
-    # plot = PlotlyLight.Plot()
-    # plot.layout.yaxis.title = "Distance"
-    # plot.layout.xaxis.title = "Pressure"
-    # plot.layout.xaxis.side = "top"
-    # plot.layout.margin.pad = 10
 
     times = Float64[]
     scalars = []
@@ -344,19 +203,47 @@ function transdec_pool_pulse(freq = 100)
             ptrans[j] = dot(ip[2], p[ip[1]])
         end
         push!(transects, ptrans)
-        # plot(x = vec(distances), y = vec(ptrans), mode="lines")
-        # display(plot)
         push!(times, t)
     end
-    vtkwritecollection("transdec_pool_pulse-f=$(freq)-p", fens, fes, times; scalars = scalars)
     d = Dict("times" => times,
             "distances" => distances,
             "transects" => transects
         )
-    DataDrop.store_json("transdec_pool_pulse-f=$(freq)-ptrans", d)
+    DataDrop.store_json("$(name)-f=$(freq)-ptrans", d)
+
+    vtkwritecollection("$(name)-f=$(freq)-p", fens, fes, times; scalars = scalars)
 
     true
 end #
+
+function plot_transects(f)
+    d = DataDrop.retrieve_json(f)
+    ptrans = d["transects"]
+    distances = d["distances"]
+    gr()
+    # plot = PlotlyLight.Plot()
+    # plot.layout.yaxis.title = "Distance"
+    # plot.layout.xaxis.title = "Pressure"
+    # plot.layout.xaxis.side = "top"
+    # plot.layout.margin.pad = 10
+    p = ptrans[1]
+    pl = plot(vec(distances), vec(p), leg = false, ylims = (-1000, 1000))
+    for i in eachindex(d["times"])
+        p = ptrans[i]
+        pl = plot(vec(distances), vec(p), leg = false, ylims = (-1000, 1000))
+        sleep(0.1)
+        display(pl)
+    end
+    # gif(anim, "anim_gr_ref002.gif")
+end
+
+function transdec_pool_pulse(freq = 100)
+    _run_transdec_pool("transdec_pool_pulse", freq, _pressure_pulse, _pressure_pulse, 2.2)
+end
+
+function transdec_pool_cw(freq = 100)
+    _run_transdec_pool("transdec_pool_cw", freq, _pressure_cw, _pressure_cw, 3)
+end
 
 function allrun()
     println("#####################################################") 
@@ -366,7 +253,8 @@ function allrun()
     # transdec_pool_harmonic(500)
     # transdec_pool_harmonic(750)
     # transdec_pool_harmonic(1000)
-    transdec_pool_pulse(100)
+    # transdec_pool_pulse(100)
+    transdec_pool_cw(100)
     return true
 end # function allrun
 
