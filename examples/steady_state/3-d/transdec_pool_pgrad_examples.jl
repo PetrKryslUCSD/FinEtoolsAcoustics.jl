63fef9a343b518888454010902611591f3c8101b
Petr Krysl <pkrysl@ucsd.edu>
2023-12-15 07:58:12 -0800

    update Pool example

--- examples/steady_state/3-d/transdec_pool_pgrad_examples.jl
+++ examples/steady_state/3-d/transdec_pool_pgrad_examples.jl
@@ -92,7 +92,7 @@ function transdec_pool_example(freq = [100])
     rho = 1002.0 * phun("kg/m^3")# mass density
     c = 1490.0 * phun("m/s")# sound speed
     bulk = c^2 * rho
-    d_piston = 0.1 * phun("mm")     # amplitude of the piston acceleration
+    v_piston = 1000 * phun("Pa")  / (rho * c)   # amplitude of the piston acceleration
     # Definition of the transect
     from = [-0.01524000021336, 1.8288000000000002, 17.2669204572]
     to = from .+ [-7.5, 0, 0]
@@ -110,12 +110,6 @@ function transdec_pool_example(freq = [100])
 
     ebc1 = FDataDict("node_list" => connectednodes(free_surface_fes), "pressure" => x -> 0.0)
 
-    function inspector(idat, i, conn, xe, out, loc)
-        qpgrads = idat
-        qpgrads[i, :] .= out[:]
-        return qpgrads
-    end
-
     qpgrads = zeros(ComplexF64, count(fes), 3)
     for f in freq
         @info "Frequency $f"
@@ -123,11 +117,11 @@ function transdec_pool_example(freq = [100])
         # Surface of the piston
         flux_pos = FDataDict(
             "femm" => FEMMAcoustSurf(IntegDomain(piston_pos_fes, TriRule(3)), material),
-            "normal_flux" => 1.0im * rho * d_piston * omega^2,
+            "normal_flux" => 1.0im * rho * v_piston * omega,
             )
         flux_neg = FDataDict(
             "femm" => FEMMAcoustSurf(IntegDomain(piston_neg_fes, TriRule(3)), material),
-            "normal_flux" => 1.0im * rho * d_piston * omega^2,
+            "normal_flux" => 1.0im * rho * v_piston * omega,
             )
         modeldata = FDataDict(
             "fens" => fens,
@@ -142,9 +136,16 @@ function transdec_pool_example(freq = [100])
         P = modeldata["P"]
 
         qpgrads .= zero(eltype(P.values))
-        idat = inspectintegpoints(region1["femm"], geom, P, NodalField([1.0]), collect(1:count(fes)), inspector, qpgrads, :gradient)
-        qpgrads = idat
+        idat = inspectintegpoints(region1["femm"], geom, P,
+            NodalField([1.0]), collect(1:count(fes)),
+            (idat, i, conn, xe, out, loc) -> let
+                qpgrads = idat
+                qpgrads[i, :] .= out[:]
+                return qpgrads
+            end, qpgrads, :gradient)
+        qpgrads = idat ./ region1["femm"].integdomain.integration_rule.npts
 
+        # The integral is only over the neighborhood of the skull; everything else is blanked out
         volf = ElementalField(zeros(size(qpgrads, 1), 1))
         volf.values[skull_subset, :] .= 1.0
         gradf = ElementalField(zeros(eltype(P.values), size(qpgrads)))
@@ -161,59 +162,21 @@ function transdec_pool_example(freq = [100])
         File = "transdec-pool-f=$(f)-pgrad.json"
         DataDrop.store_json(File, Dict("grad" => [g1, g2, g3] ./ vol))
 
-        File = "transdec-pool-f=$(f)-p.vtk"
-        vtkexportmesh(
-            File,
-            fens,
-            fes;
-            scalars = [
-            ("absP", abs.(P.values)),
-            ("realP", real.(P.values)),
-            ("imagP", imag.(P.values)),
-            ],
-            )
+        # File = "transdec-pool-f=$(f)-p.vtk"
+        # vtkexportmesh(
+        #     File,
+        #     fens,
+        #     fes;
+        #     scalars = [
+        #     ("absP", abs.(P.values)),
+        #     ("realP", real.(P.values)),
+        #     ("imagP", imag.(P.values)),
+        #     ],
+        #     )
     # @async run(`"paraview.exe" $File`)
     end
     true
 end #
-
-#=
-plot_transects = Main.transdec_pool_examples.plot_transects
-pl = nothing
-for f in 440:1:520;
-   plot_transects(pl, "transdec-pool-f=$f-pgtrans.json", f);
-   sleep(1)
-end
-    =#
-function plot_transects(pl, f, freq)
-    d = DataDrop.retrieve_json(f)
-    pgtrans = d["pgtrans"]
-    distances = d["distances"]
-    gr()
-    if pl == nothing
-        pl = plot(
-            vec(distances),
-            vec(pgtrans),
-            leg = false,
-            ylims = (0, 1000000),
-            title = "Frequency: $(freq)",
-            xaxis = ("Distance along transect",),
-            yaxis = ("||Pressure gradient||",),
-            )
-    else
-        pl = plot!(pl,
-            vec(distances),
-            vec(pgtrans),
-            leg = false,
-            ylims = (0, 100000),
-            title = "Frequency: $(freq)",
-            xaxis = ("Distance along transect",),
-            yaxis = ("||Pressure gradient||",),
-            )
-    end
-    display(pl)
-    return pl
-end
 
 function allrun()
     println("#####################################################")
@@ -223,7 +186,7 @@ function allrun()
     # transdec_pool_example(500)
     # transdec_pool_example(750)
     # transdec_pool_example(1000)
-    freq = collect(430:1:430)
+    freq = collect(170:2:1000)
     transdec_pool_example(freq)
 
     return true
@@ -232,5 +195,5 @@ end # function allrun
 @info "All examples may be executed with "
 println("using .$(@__MODULE__); $(@__MODULE__).allrun()")
 
-end # module baffled_piston_examples
+end # module
 nothing
